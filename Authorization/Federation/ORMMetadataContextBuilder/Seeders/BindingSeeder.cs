42cfeaad81f23e28c1abae42cf712470a767bb0f
dg-mfb <danail.georgiev73@gmail.com>
2017-09-28 09:28:16 +0100

    adding metadata handler setu up

--- Authorization/Federation/ORMMetadataContextBuilder/Seeders/BindingSeeder.cs
+++ Authorization/Federation/ORMMetadataContextBuilder/Seeders/BindingSeeder.cs
@@ -1,4 +1,5 @@
 ﻿using Kernel.Data.ORM;
+using Kernel.Federation.MetaData.Configuration;
 using ORMMetadataContextProvider.Models;
 
 namespace ORMMetadataContextProvider.Seeders
@@ -7,9 +8,9 @@ namespace ORMMetadataContextProvider.Seeders
     {
         public override void Seed(IDbContext context)
         {
-            var redirectBinding = new Binding { Uri = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect", Name = "HTTP-Redirect" };
+            var redirectBinding = new Binding { Uri = Bindings.Http_Redirect, Name = "HTTP-Redirect" };
             context.Add<Binding>(redirectBinding);
-            var postBinding = new Binding { Uri = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST", Name = "HTTP-POST" };
+            var postBinding = new Binding { Uri = Bindings.Http_Post, Name = "HTTP-POST" };
             context.Add<Binding>(postBinding);
             Seeder._cache.Add(Seeder.BindingsKey, new[] { redirectBinding, postBinding });
         }
---
+++ Kernel/Kernel.Federation/MetaData/IMetadataHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kernel.Federation.MetaData
+{
+    public interface IMetadataHandler<TMetadata>
+    {
+        Uri ReadIdpLocation(Uri binding);
+    }
+}
