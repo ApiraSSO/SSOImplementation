1af67cd4a89b9b34448b1f404b89ab1c0c45a76d
dg-mfb <danail.georgiev73@gmail.com>
2017-10-03 08:32:44 +0100

    relay state handler and serialisr tests

---
+++ Authorization/Federation/Federation.Protocols.Test/RelayState/RelayStateHandlerTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DeflateCompression;
+using Federation.Protocols.Endocing;
+using Federation.Protocols.RelayState;
+using Kernel.Federation.Protocols;
+using NUnit.Framework;
+
+namespace Federation.Protocols.Test.RelayState
+{
+    [TestFixture]
+    internal class RelayStateHandlerTest
+    {
+        [Test]
+        public async Task HandlerTest()
+        {
+            //ARRANGE
+            var relayState = "Test state";
+            var form = new Dictionary<string, string>();
+            var compressor = new DeflateCompressor();
+            var messageEncoder = new MessageEncoding(compressor);
+            var serialiser = new RelaystateSerialiser(messageEncoder) as IRelayStateSerialiser;
+            var handler = new RelayStateHandler(serialiser);
+            //ACT
+            var serialised = serialiser.Serialize(relayState);
+            form.Add("RelayState", serialised);
+            var deserialised = await handler.GetRelayStateFromFormData(form);
+            //ASSERT
+            Assert.AreEqual(relayState, deserialised);
+        }
+    }
+}
---
+++ Authorization/Federation/Federation.Protocols.Test/RelayState/RelaystateSerialiserTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using DeflateCompression;
+using Federation.Protocols.Endocing;
+using Federation.Protocols.RelayState;
+using Kernel.Federation.Protocols;
+using NUnit.Framework;
+
+namespace Federation.Protocols.Test.RelayState
+{
+    [TestFixture]
+    internal class RelayStateSerialiserTest
+    {
+        [Test]
+        public async Task SerialiseDeserialiseTest()
+        {
+            //ARRANGE
+            var relayState = "Test state";
+            var compressor = new DeflateCompressor();
+            var messageEncoder = new MessageEncoding(compressor);
+            var serialiser = new RelaystateSerialiser(messageEncoder) as IRelayStateSerialiser;
+            //ACT
+            var serialised = serialiser.Serialize(relayState);
+            var deserialised = await serialiser.Deserialize(serialised);
+            //ASSERT
+            Assert.AreEqual(relayState, deserialised);
+        }
+    }
+}
