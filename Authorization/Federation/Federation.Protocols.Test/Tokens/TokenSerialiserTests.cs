0de14bf8aa207187c5994dd7525be92088eaae6d
dg-mfb <danail.georgiev73@gmail.com>
2017-10-04 16:02:21 +0100

    adding raw read token tests

--- Authorization/Federation/Federation.Protocols.Test/Tokens/TokenSerialiserTests.cs
+++ Authorization/Federation/Federation.Protocols.Test/Tokens/TokenSerialiserTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Xml;
 using Federation.Protocols.Test.Mock;
+using Federation.Protocols.Test.Mock.Tokens;
 using Federation.Protocols.Tokens;
 using NUnit.Framework;
 
@@ -39,12 +40,32 @@ namespace Federation.Protocols.Test.Tokens
             var tokenHandlerConfigurationProvider = new TokenHandlerConfigurationProvider(federationPartyContextBuilder, certValidator);
 
             var tokenSerialiser = new TokenSerialiser(tokenHandlerConfigurationProvider);
-
+           
             //ACT
             var token = tokenSerialiser.DeserialiseToken(reader, "testshib");
 
             //Assert
             Assert.NotNull(token);
+        }
+
+        [Test]
+        public void DeserialiseTokenTest_signed_only_assertion_raw()
+        {
+            //ARRANGE
+            var path = @"D:\Dan\Software\Apira\Assertions\FromLocal\20171041550.xml";
+            var certValidator = new CertificateValidatorMock();
+            var federationPartyContextBuilder = new FederationPartyContextBuilderMock();
+            var xmlReader = XmlReader.Create(path);
+            var reader = XmlReader.Create(xmlReader, xmlReader.Settings);
+            var tokenHandlerConfigurationProvider = new TokenHandlerConfigurationProvider(federationPartyContextBuilder, certValidator);
+            var configuration = tokenHandlerConfigurationProvider.GetConfiguration("testshib");
+            var saml2SecurityTokenHandler = new SecurityTokenHandlerMock();
+            saml2SecurityTokenHandler.SetConfiguration(configuration);
+            //ACT
+            var assertion = saml2SecurityTokenHandler.GetAssertion(reader);
+
+            //Assert
+            Assert.NotNull(assertion);
         }
     }
 }
