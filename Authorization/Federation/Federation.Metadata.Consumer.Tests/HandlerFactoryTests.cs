af3bf7c76569d540139a8bc7be7f4e9571965921
dg-mfb <danail.georgiev73@gmail.com>
2017-10-06 14:13:41 +0100

    HandlerFactoryTests

---
+++ Authorization/Federation/Federation.Metadata.Consumer.Tests/HandlerFactoryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Metadata;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Federation.Metadata.Consumer.Tests.Mock;
+using Federation.Metadata.FederationPartner.Handlers;
+using NUnit.Framework;
+
+namespace Federation.Metadata.Consumer.Tests
+{
+    [TestFixture]
+    internal class HandlerFactoryTests
+    {
+        [Test]
+        public void GetDelegateForIdpDescriptors_entity_descriptor_metadata_Test()
+        {
+            //ARRANGE
+            var metadata = EntityDescriptorProviderMock.GetEntityDescriptor();
+            var handler = new MetadataEntitityDescriptorHandler();
+            //ACT
+            var del = HandlerFactory.GetDelegateForIdpDescriptors(typeof(EntityDescriptor), typeof(IdentityProviderSingleSignOnDescriptor));
+            var idps = del(handler, metadata)
+                .ToList();
+            //ASSERT
+            Assert.AreEqual(1, idps.Count);
+        }
+
+        [Test]
+        public void GetDelegateForIdpDescriptors_entities_descriptor_metadata_Test()
+        {
+            //ARRANGE
+            var metadata = EntityDescriptorProviderMock.GetEntitiesDescriptor();
+            var handler = new MetadataEntitiesDescriptorHandler();
+            //ACT
+            var del = HandlerFactory.GetDelegateForIdpDescriptors(typeof(EntitiesDescriptor), typeof(IdentityProviderSingleSignOnDescriptor));
+            var idps = del(handler, metadata)
+                .ToList();
+            //ASSERT
+            Assert.AreEqual(1, idps.Count);
+        }
+    }
+}
---
+++ Authorization/Federation/Federation.Metadata.Consumer.Tests/Mock/EntityDescriptorProviderMock.cs
@@ -0,0 +1,23 @@
+﻿using System.IdentityModel.Metadata;
+
+namespace Federation.Metadata.Consumer.Tests.Mock
+{
+    public class EntityDescriptorProviderMock
+    {
+        public static EntityDescriptor GetEntityDescriptor()
+        {
+            var descriptor = new EntityDescriptor();
+            var idpRole = new IdentityProviderSingleSignOnDescriptor();
+            descriptor.RoleDescriptors.Add(idpRole);
+            return descriptor;
+        }
+
+        public static EntitiesDescriptor GetEntitiesDescriptor()
+        {
+            var descriptor = new EntitiesDescriptor();
+            var entityDesc = EntityDescriptorProviderMock.GetEntityDescriptor();
+            descriptor.ChildEntities.Add(entityDesc);
+            return descriptor;
+        }
+    }
+}
