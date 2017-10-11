ef1ff2f697708fefa5c1701d711c9e854e3ecd28
dg-mfb <danail.georgiev73@gmail.com>
2017-10-11 11:40:10 +0100

    authn request test set up

--- Authorization/Federation/Federation.Protocols.Test/AuthnRequestTests.cs
+++ Authorization/Federation/Federation.Protocols.Test/AuthnRequestTests.cs
@@ -14,71 +14,21 @@ namespace Federation.Protocols.Test
     public class AuthnRequestTests
     {
         [Test]
-        public async Task BuildAuthnRequest_test()
+        public void BuildAuthnRequest_test()
         {
-            throw new NotImplementedException();
             //ARRANGE
-            //var requestUri = new Uri("http://localhost:59611/");
-            //var federationPartyContextBuilder = new FederationPartyContextBuilderMock();
-            //var federationContex = federationPartyContextBuilder.BuildContext("local");
-            //var authnRequestContext = new AuthnRequestContext(requestUri, federationContex);
+            var requestUri = new Uri("http://localhost:59611/");
+            var federationPartyContextBuilder = new FederationPartyContextBuilderMock();
+            var federationContex = federationPartyContextBuilder.BuildContext("local");
+            var authnRequestContext = new AuthnRequestContext(requestUri, federationContex);
             
-            //var serialiser = new XMLSerialiser();
-            //var certManager = new CertificateManager();
-            //var authnRequest = AuthnRequestHelper.BuildAuthnRequest(authnRequestContext);
-            //var compressor = new DeflateCompressor();
-            //var certContext = new X509CertificateContext
-            //{
-            //    StoreLocation = StoreLocation.LocalMachine,
-            //    ValidOnly = false,
-            //    StoreName = "TestCertStore"
-            //};
-            //certContext.SearchCriteria.Add(new CertificateSearchCriteria
-            //{
-            //    SearchCriteriaType = X509FindType.FindBySubjectName,
-            //    SearchValue = "ApiraTestCertificate"
-            //});
-            ////ACT
-            //var sb = new StringBuilder();
-            ////serialise and append request
-            //var serialised =  AuthnRequestHelper.Serialise(authnRequest, serialiser);
-            //await AuthnRequestHelper.AppendRequest(sb, serialised, compressor);
+            //ACT
+            var authnRequest = AuthnRequestHelper.BuildAuthnRequest(authnRequestContext);
 
-            ////append relying state
-            //await AuthnRequestHelper.AppendRelyingState(sb, authnRequestContext, compressor);
+            //ASSERT
+            Assert.NotNull(authnRequest);
+            Assert.AreEqual("2.0", authnRequest.Version);
 
-            ////append signature alg
-            //AuthnRequestHelper.AppendSignarureAlgorithm(sb);
-
-            //var datataToSign = sb.ToString();
-
-            ////sign request
-            //AuthnRequestHelper.SignData(sb, certContext, certManager);
-            //var query = sb.ToString();
-            //var url = new Uri(String.Format("{0}?{1}", requestUri, query));
-            //var qsParsed = HttpUtility.ParseQueryString(url.Query);
-            ////request
-            //var requestEscaped = qsParsed[0];
-            //var requestUnescaped = Uri.UnescapeDataString(requestEscaped);
-            //var requestDecompressed = await Helper.DeflateDecompress(requestUnescaped, compressor);
-
-            ////relyingState
-            //var rsEscaped = qsParsed[1];
-            //var rsunescaped = Uri.UnescapeDataString(rsEscaped);
-            //var rsdecompressed = await Helper.DeflateDecompress(rsunescaped, compressor);
-            
-            ////signature alg
-            //var sigAlgEscaped = qsParsed[2];
-            //var sigAlgunescaped = Uri.UnescapeDataString(sigAlgEscaped);
-
-            ////signature
-            //var sigEscaped = qsParsed[3];
-            //var sigunescaped = Uri.UnescapeDataString(sigEscaped);
-
-            ////signed string
-            //var verified = certManager.VerifySignatureFromBase64(datataToSign, sigEscaped, certContext);
-            ////ASSERT
-            //Assert.True(verified);
         }
 
         [Test]
