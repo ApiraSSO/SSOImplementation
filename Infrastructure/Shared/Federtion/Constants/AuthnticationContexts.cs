a40cd69b2a0f5393271f472b2ba17ea88b833293
dg-mfb <danail.georgiev73@gmail.com>
2017-10-13 17:31:58 +0100

    requested authntication context builder implmenetation

---
+++ Infrastructure/Shared/Federtion/Constants/AuthnticationContexts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Federtion.Constants
+{
+    public class AuthnticationContexts
+    {
+        public const string Password = "urn:oasis:names:tc:SAML:2.0:ac:classes:Password";
+        public const string PasswordProtectedTransport = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport";
+    }
+}
