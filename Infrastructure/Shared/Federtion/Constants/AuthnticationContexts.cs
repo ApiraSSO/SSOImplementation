37e5ed43bb1dc070f40cf84a53d188c97d2140cc
dg-mfb <danail.georgiev73@gmail.com>
2017-10-13 17:44:00 +0100

    adding unspecified authn context

--- Infrastructure/Shared/Federtion/Constants/AuthnticationContexts.cs
+++ Infrastructure/Shared/Federtion/Constants/AuthnticationContexts.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shared.Federtion.Constants
+﻿namespace Shared.Federtion.Constants
 {
     public class AuthnticationContexts
     {
         public const string Password = "urn:oasis:names:tc:SAML:2.0:ac:classes:Password";
         public const string PasswordProtectedTransport = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport";
+        public const string Unspecified = "urn:oasis:names:tc:SAML:2.0:ac:classes:unspecified";
     }
 }
